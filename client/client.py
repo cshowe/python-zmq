4e2299f3e03f027e0abee54a57aa3d9682622f51
Caleb Howe <caleb@kensho.com>
2015-02-19 22:39:14 -0500

    Ncurses and slightly better exit code

--- client/client.py
+++ client/client.py
@@ -1,12 +1,40 @@
-import zmq
-import time
+import curses
+import zmq.green as zmq
+import gevent
+import sys
 
 context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect('tcp://127.0.0.1:5555')
-while True:
-    print 'PING'
-    socket.send_string('PING')
-    response = socket.recv_string()
-    print response
-    time.sleep(2)
+
+window = curses.initscr()
+curses.noecho()
+curses.cbreak()
+window.keypad(1)
+
+def hb():
+    hb_socket = context.socket(zmq.REQ)
+    hb_socket.connect('tcp://127.0.0.1:5555')
+    try:
+        while True:
+            hb_socket.send_string('PING')
+            response = hb_socket.recv_string()
+            gevent.sleep(2)
+    except gevent.GreenletExit:
+        return
+
+def gevent_getch(window):
+    gevent.select.select([sys.stdin], [], [])
+    return window.getch()
+
+def input_loop(window):
+    while True:
+         keypress = gevent_getch(window)
+         if keypress == ord('q'):
+             return
+         window.addch(keypress)
+         window.refresh()
+
+
+if __name__ == '__main__':
+    hb_greenlet = gevent.spawn(hb)
+    curses.wrapper(input_loop)
+    hb_greenlet.kill()
