845eb6fde7a06582a25b57550cec7b9f4445c07d
Caleb Howe <caleb@kensho.com>
2015-02-25 22:11:24 -0500

    Generalize entities

--- server/server.py
+++ server/server.py
@@ -9,19 +9,23 @@ class Scheduler(object):
 
 
     def handle_event(self, agent, action):
-        self._gamestate.apply_action(action)
+        self._gamestate.apply_action(agent, action)
         new_action = agent.get_action(self._gamestate)
         if new_action is not None:
             self._scheduler.enter(1, 0, self.handle_event, (agent, new_action))
 
-    def start(self, player):
-        self._scheduler.enter(0, 0, self.handle_event, (player, {}))
+    def spawn(self, entity):
+        self._scheduler.enter(0, 0, self.handle_event, (entity, {}))
+
+    def start(self):
         self._scheduler.run()
 
 
 class Player(object):
-    def __init__(self, socket):
+    def __init__(self, x, y, socket):
         self._socket = socket
+        self.x = x
+        self.y = y
 
     def get_action(self, gamestate):
         self._socket.send_json(gamestate.to_json())
@@ -31,8 +35,9 @@ class Player(object):
 
 
 class Bob(object):
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
     def get_action(self, gamestate):
         if gamestate._quit:
@@ -46,9 +51,8 @@ class Bob(object):
 class GameState(object):
     def __init__(self, size_x, size_y):
         self._time = 0
-        self._player_x = 5
-        self._player_y = 5
         self._map = []
+        self._entities = []
         self._quit = False
         for i in range(size_y):
             if i == 0 or i == size_y - 1:
@@ -62,15 +66,18 @@ class GameState(object):
     def delay(self, delay):
         self._time += delay
 
-    def apply_action(self, action):
+    def add_entity(self, entity):
+        self._entities.append(entity)
+
+    def apply_action(self, entity, action):
         if action.get('type', 'NOP') == 'QUIT':
             self._quit = True
         elif action.get('type', 'NOP') == 'MOVE':
-            nx = self._player_x + action.get('dx', 0)
-            ny = self._player_y + action.get('dy', 0)
+            nx = entity.x + action.get('dx', 0)
+            ny = entity.y + action.get('dy', 0)
             if self._map[ny][nx] == '.':
-                self._player_x = nx
-                self._player_y = ny
+                entity.x = nx
+                entity.y = ny
 
     def to_json(self):
         if self._quit:
@@ -82,8 +89,7 @@ class GameState(object):
                 'status': 'Time %d' % self._time,
                 'entities': [{
                     'type': 'player',
-                    'x': self._player_x,
-                    'y': self._player_y}]}
+                    'x': e.x, 'y': e.y} for e in self._entities]}
 
 
 if __name__ == '__main__':
@@ -93,6 +99,11 @@ if __name__ == '__main__':
     config = socket.recv_json()
     gamestate = GameState(config['size_x'], config['size_y'])
     scheduler = Scheduler(gamestate)
-    player = Player(socket)
-    scheduler.start(player)
+    player = Player(5, 5, socket)
+    bob = Bob(3, 20)
+    gamestate.add_entity(player)
+    gamestate.add_entity(bob)
+    scheduler.spawn(player)
+    scheduler.spawn(bob)
+    scheduler.start()
 
